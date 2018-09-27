503843508f422abd04f95768e76afb6cbbe75aba
Tomoki, Hirayama <ar-tomoki.hirayama@rakuten.com>
2018-09-27 11:36:06 +0900

    Add prj

---
+++ RealTimeChatApplicationSample/Pods/Target Support Files/Pods-RealTimeChatApplicationSample/Pods-RealTimeChatApplicationSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealTimeChatApplicationSample : NSObject
+@end
+@implementation PodsDummy_Pods_RealTimeChatApplicationSample
+@end
---
+++ RealTimeChatApplicationSample/Pods/Target Support Files/Pods-RealTimeChatApplicationSampleTests/Pods-RealTimeChatApplicationSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealTimeChatApplicationSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_RealTimeChatApplicationSampleTests
+@end
